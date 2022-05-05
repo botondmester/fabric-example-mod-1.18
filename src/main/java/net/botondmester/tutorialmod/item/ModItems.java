7247119e85c6ba49558a32b23b812addc32d050e
botondmester <boboooka1@gmail.com>
2022-05-05 20:45:12 +0200

    added mythril_ingot

--- src/main/java/net/botondmester/tutorialmod/item/ModItems.java
+++ src/main/java/net/botondmester/tutorialmod/item/ModItems.java
@@ -1,5 +1,23 @@
 package net.botondmester.tutorialmod.item;
 
-public class ModItems {
+import net.botondmester.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
+import static net.botondmester.tutorialmod.TutorialMod.MOD_ID;
+
+public class ModItems {
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + MOD_ID);
+    }
 }
